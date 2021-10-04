fc6b461102887cd1cc01293fb36c220029b494f1
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-10-04 14:31:04 -0300

    :fix: Adding LandingPage (Teste)

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Never Stop Learning Luis Felipe Garção Silva
         </a>
       </header>
     </div>
